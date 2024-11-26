4aafae8791d0640be11c197e2972f1272d0e85e0
Facundo <facund097dv@gmail.com>
2024-11-26 10:51:34 -0300

    add environment variable PORT for server configuration

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const PORT = 3001;
 
 let persons = [
     {
@@ -130,6 +129,7 @@ app.put("/api/persons/:id", (request, response) => {
     response.sendStatus(200);
 });
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server starting in PORT ${PORT}`);
 });
